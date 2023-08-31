250aadfb200e66d57d6aae2acf68605a16633a9d
Jesiel Palacios <jopalacioss@unac.edu.co>
2023-08-30 21:12:50 -0500

    Initial commit

---
+++ src/main/java/com/jesielvirtual/sistemagestionclientes/SistemaGestionClientes.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.jesielvirtual.sistemagestionclientes;
+
+/**
+ *
+ * @author jesielpalacios
+ */
+public class SistemaGestionClientes {
+
+    public static void main(String[] args) {
+        // System.out.println("Hello World!");
+        Formulario ventana = new Formulario();
+
+        ventana.setVisible(true);
+    }
+}
